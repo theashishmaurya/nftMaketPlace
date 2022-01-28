89ccca139078a72b0f45081f3fa3cde4e7b8d8d8
sneha025 <simaurya96@gmail.com>
2022-01-29 00:40:01 +0530

    added fetch nft owned by address

---
+++ pages/api/fetchNFTOwnedBy.js
@@ -0,0 +1,25 @@
+import { ThirdwebSDK } from "@3rdweb/sdk";
+import { ethers } from "ethers";
+
+export default async function fetchNft(req, res) {
+  const rpcUrl = "https://rpc-mumbai.maticvigil.com";
+  const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY;
+  const NFT_MODULE_ADDRESS = "0x0C8fe5019D3B3BaC3B9e0878080C898518E02060";
+
+  const { address } = await req.boby;
+  const wallet = new ethers.Wallet(
+    privateKey,
+    ethers.getDefaultProvider(rpcUrl)
+  );
+
+  const nft = new ThirdwebSDK(wallet).getNFTModule(NFT_MODULE_ADDRESS);
+  // nft owned by address
+  await nft
+    .getOwned(address)
+    .then((data) => {
+      res.send(JSON.stringify(data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
